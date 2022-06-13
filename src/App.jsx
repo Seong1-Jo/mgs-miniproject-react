2653facf535d21edfee54394cd9d17860d9ea709
youngcodej22 <youngcodej22@gmail.com>
2022-06-13 15:50:10 +0900

    feat: Add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Home from './pages/Home'
+
+// 이거 확인하시고 지우시면 됩니다.
+// Styled Component 
+// font-family:
+// https://fonts.google.com/noto/specimen/Noto+Sans?query=noto+sans
+// font-size: 14px 기본
+// background-color: #fff
+// reset
+// https://www.npmjs.com/package/styled-reset
 
 function App() {
   return (
     <>
       <div>
-        <h1>hello team6</h1>
+        <Home />
       </div>
     </>
     
